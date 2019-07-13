1fc21e64da63fe909af3f961cdaf328e0c65e5f1
Everton Zoelner <everton.zoelner@gmail.com>
2019-07-13 15:31:06 -0300

    Buscando produtos da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,90 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapatenis-polo-blu-casual-masculino/06/DQH-0012-006/DQH-0012-006_detalhe2.jpg?resize=326:*"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,00</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapatenis-polo-blu-casual-masculino/06/DQH-0012-006/DQH-0012-006_detalhe2.jpg?resize=326:*"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,00</span>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+    this.setState({
+      products: data,
+    });
+  }
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapatenis-polo-blu-casual-masculino/06/DQH-0012-006/DQH-0012-006_detalhe2.jpg?resize=326:*"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,00</span>
+  render() {
+    const { products } = this.state;
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+    return (
+      <ProductList>
+        {products.map(product => {
+          return (
+            <li key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <strong>{product.title}</strong>
+              <span>{product.priceFormatted}</span>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapatenis-polo-blu-casual-masculino/06/DQH-0012-006/DQH-0012-006_detalhe2.jpg?resize=326:*"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,00</span>
+              <button type="button">
+                <div>
+                  <MdAddShoppingCart size={16} color="#fff" /> 3
+                </div>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/sapatenis-polo-blu-casual-masculino/06/DQH-0012-006/DQH-0012-006_detalhe2.jpg?resize=326:*"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+                <span>ADICIONAR AO CARRINHO</span>
+              </button>
+            </li>
+          );
+        })}
+      </ProductList>
+    );
+  }
 }
