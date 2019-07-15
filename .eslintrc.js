594f00621510c468fb399f6ba46e3c5e2d699367
Everton Zoelner <everton.zoelner@gmail.com>
2019-07-15 02:57:29 -0300

    Convertendo classe

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,11 +16,13 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'no-console': ['error', { allow: 'tron' }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
